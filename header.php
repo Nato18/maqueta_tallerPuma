54cc7669e20229f36d8f45c9ba132c1f44ae2413
Renato Parraguez Rubilar <rena_010@hotmail.com>
2023-07-26 17:18:49 -0400

    primer commit para la maqueta

---
+++ header.php
@@ -0,0 +1,16 @@
+<body>
+    <div class="main-nav bg-2">
+        <div class="nav-body">
+            <div></div>
+            <div class="nav-bar">
+                <div class="nav-item os-400 text-black f-20"><a class=" a-select">Crea Tu cuadro</a></div>
+                <div class="nav-item os-400 text-black f-20"><a href="">Servicios</a></div>
+                <div class="nav-item os-400 text-black f-20"><a href="">Insumos</a></div>
+                <div class="nav-item os-400 text-black f-20"><a href="">Quienes Somos</a></div>
+                <div class="nav-item os-400 text-black f-20"><a href="">Imagen</a></div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Retela</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+
+<body>
+    <?php
+    include 'header.php';
+    ?>
+    <div class="separacion-cuadro">
+        <div>
+            <div class="text-center os-700 f-24 p-10">Crea tu Cuadro</div>
+            <div class="">
+                <ul class="progreso">
+                    <li class="step-actual os-700 f-20">1</li>
+                    <li class="step-prox os-700 f-20">2</li>
+                    <li class="step-prox os-700 f-20">3</li>
+                    <li class="step-prox os-700 f-20">4</li>
+                    <li class="step-prox os-700 f-20">5</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="bg-3 vista-cuadro">
+            <div>hola</div>
+        </div>
+    </div>
+
+
+    <?php
+    include 'footer.php';
+    ?>
+</body>
+
+</html>
